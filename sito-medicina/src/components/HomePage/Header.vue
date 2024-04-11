a1981753cb0861e15c87040523b7e0a4cb9b0653
MarcoSalvatore91 <marcosalvatore91@hotmail.com>
2024-04-11 23:35:00 +0200

    header

---
+++ sito-medicina/src/components/HomePage/Header.vue
@@ -0,0 +1,21 @@
+<template>
+  <section id="homepage">
+    <header>
+      <img id="img-logo" src="@/assets/logo.svg" alt="logo" />
+    </header>
+    <main></main>
+    <footer></footer>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'Header'
+}
+</script>
+
+<style scoped lang="scss">
+header #img-logo {
+  height: 20px;
+}
+</style>
---
+++ sito-medicina/src/views/HomePage.vue
@@ -0,0 +1,19 @@
+<template>
+  <section id="homepage">
+    <Header />
+  </section>
+</template>
+
+<script>
+import Header from '../components/HomePage/Header.vue'
+
+export default {
+  name: 'HomePage',
+
+  components: {
+    Header
+  }
+}
+</script>
+
+<style></style>
