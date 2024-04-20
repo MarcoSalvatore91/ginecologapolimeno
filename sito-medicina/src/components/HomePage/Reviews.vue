a036b7515d3374fb5bf5637326dd9b4afa71aed0
MarcoSalvatore91 <marcosalvatore91@hotmail.com>
2024-04-20 19:01:08 +0200

    reviews end

--- sito-medicina/src/components/HomePage/Reviews.vue
+++ sito-medicina/src/components/HomePage/Reviews.vue
@@ -1,16 +1,75 @@
 <template>
   <section id="reviews">
-    <div id="reviews-container"></div>
-    <h2 id="title-reviews">Recensioni Dei Nostri Pazienti</h2>
+    <div id="info-review">
+      <div id="reviews-container"></div>
+      <h2 id="title-reviews">Recensioni Dei Nostri Pazienti</h2>
+    </div>
+
+    <Carousel>
+      <Slide v-for="review in reviews" :key="review">
+        <div class="carousel__item">
+          <div>
+            <h3>{{ review.name }}</h3>
+            <b-icon-star-fill v-for="star in 5" :key="star"></b-icon-star-fill>
+          </div>
+          <div>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit minus et saepe vitae
+            asperiores impedit doloribus fuga labore nihil beatae! Pariatur, sint aspernatur.
+            Assumenda aperiam neque inventore porro laborum itaque!
+          </div>
+        </div>
+      </Slide>
+
+      <template #addons>
+        <Pagination />
+      </template>
+    </Carousel>
   </section>
 </template>
 
 <script>
+import { Carousel, Navigation, Pagination, Slide } from 'vue3-carousel'
+import 'vue3-carousel/dist/carousel.css'
+
 export default {
   name: 'Reviews',
+  components: {
+    Carousel,
+    Slide,
+    Pagination,
+    Navigation
+  },
 
   data() {
-    return {}
+    return {
+      reviews: [
+        {
+          name: 'User',
+          stars: 5,
+          text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit minus et saepe vitae asperiores impedit doloribus fuga labore nihil beatae! Pariatur, sint aspernatur. Assumenda aperiam neque inventore porro laborum itaque!'
+        },
+        {
+          name: 'User',
+          stars: 5,
+          text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit minus et saepe vitae asperiores impedit doloribus fuga labore nihil beatae! Pariatur, sint aspernatur. Assumenda aperiam neque inventore porro laborum itaque!'
+        },
+        {
+          name: 'User',
+          stars: 5,
+          text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit minus et saepe vitae asperiores impedit doloribus fuga labore nihil beatae! Pariatur, sint aspernatur. Assumenda aperiam neque inventore porro laborum itaque!'
+        },
+        {
+          name: 'User',
+          stars: 5,
+          text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit minus et saepe vitae asperiores impedit doloribus fuga labore nihil beatae! Pariatur, sint aspernatur. Assumenda aperiam neque inventore porro laborum itaque!'
+        },
+        {
+          name: 'User',
+          stars: 5,
+          text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit minus et saepe vitae asperiores impedit doloribus fuga labore nihil beatae! Pariatur, sint aspernatur. Assumenda aperiam neque inventore porro laborum itaque!'
+        }
+      ]
+    }
   },
 
   methods: {}
@@ -19,13 +78,44 @@ export default {
 
 <style scoped lang="scss">
 #reviews {
-  border-top: 5px solid rgb(173, 216, 230);
-  margin: 25px 40px;
-  #reviews-container {
-    margin: 30px 0;
+  #info-review {
+    border-top: 5px solid rgb(173, 216, 230);
+    margin: 25px 40px;
+    #reviews-container {
+      margin: 30px 0;
+    }
+    #title-reviews {
+      text-align: center;
+      padding-bottom: 10px;
+    }
   }
-  #title-reviews {
-    text-align: center;
+  .carousel__item {
+    min-height: 200px;
+    width: 100%;
+    box-shadow: 0 0 10px 0 rgba(0, 0, 0, 0.15);
+    border-radius: 5px;
+    color: black;
+    font-size: 20px;
+    border-radius: 8px;
+    text-align: start;
+    padding: 10px 20px;
+    div:first-child {
+      padding: 20px 0;
+      border-bottom: 1px solid rgb(173, 216, 230);
+    }
+    div:nth-child(2) {
+      padding: 20px 0;
+    }
+  }
+
+  .carousel__slide {
+    padding: 10px 20px;
+  }
+
+  .carousel__prev,
+  .carousel__next {
+    box-sizing: content-box;
+    border: 5px solid white;
   }
 }
 </style>
--- sito-medicina/src/main.js
+++ sito-medicina/src/main.js
@@ -6,7 +6,8 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
-import BootstrapVue3 from 'bootstrap-vue-3'
+import { BootstrapVue3 } from 'bootstrap-vue-3'
+import { BootstrapIconsPlugin } from 'bootstrap-icons-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue-3/dist/bootstrap-vue-3.css'
 
@@ -15,5 +16,6 @@ const app = createApp(App)
 app.use(createPinia())
 app.use(router)
 app.use(BootstrapVue3)
+app.use(BootstrapIconsPlugin)
 
 app.mount('#app')
